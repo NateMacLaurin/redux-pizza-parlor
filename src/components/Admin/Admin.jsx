ab33c349b172da38ca8c7654cbf0d2da5800528d
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-01-21 16:21:16 -0600

    sets up table framework for admin page

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,7 +1,9 @@
 import {useHistory} from 'react-router-dom';
+import {useSelector} from 'react-redux'
 
 function Admin (){
 
+    const orders = useSelector(store => store.adminReducer)
     const history = useHistory();
     const handleReturn = () => {
         console.log(`Returning to Order Page 1:`);
@@ -9,7 +11,28 @@ function Admin (){
     }
     return(
         <>
-        <h3>Welcome to Admin Page</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Time Order Placed</th>
+                    <th>Type</th>
+                    <th>Cost</th>
+                </tr>
+            </thead>
+            <tbody>
+                {orders.map((order, i) => {
+                    return (
+                        <tr key={i}>
+                            <td>{order.name}</td>
+                            <td>{order.timePlaced}</td>
+                            <td>{order.type}</td>
+                            <td>{order.cost}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
         <button onClick={handleReturn}>FLEE YOU FOOLS</button>
         </>
     )
