1979c1cdbee25676d085c206105a620f504c1aa7
NateMacLaurin <brow1982@umn.edu>
2021-01-21 13:36:28 -0600

    added routes in react app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
-import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import {HashRouter as Router, Route} from 'react-router-dom';
+import PizzaList from '../PizzaList/PizzaList.jsx';
+import CustomerForm from '../PizzaList/CustomerForm.jsx';
+import Checkout from '../Checkout/Checkout.jsx';
+import Admin from '../Admin/Admin.jsx';
 
 function App() {
 
   return (
+    <Router>
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
-  
+      <Route path="/" exact component={PizzaList} />
+      <Route path="/pizza" component={PizzaList} />
+      <Route path="/order" component={CustomerForm} />
+      <Route path="/checkout" component={Checkout} />
+      <Route path="/admin" component={Admin} />
     </div>
+    </Router>
   );
 }
 
